50cda78d543409a1705e4ec0456eb33ee57e7941
SeanDaDon <45476609+SeanDaDon@users.noreply.github.com>
2018-11-29 20:38:22 -0800

    Add files via upload

---
+++ GSM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPtest1
+{
+    class GSM
+    {
+        public static void gsm()
+        {
+            if (MainPage.roundEnded == true)
+            {
+                MainPage.BG = MainPage.scoreScreen;
+            }
+            else
+            {
+                if (MainPage.gameState == 0)
+                {
+                    MainPage.BG = MainPage.startScreen;
+                }
+                else if (MainPage.gameState == 1)
+                {
+                    MainPage.BG = MainPage.levelScreen;
+                }
+            }
+
+        }
+    }
+}
