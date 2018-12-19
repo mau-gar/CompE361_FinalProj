4244cf72b49bff83310bc5868f4fc84cf378e42e
Mauro <45185766+mau-gar@users.noreply.github.com>
2018-12-18 16:11:56 -0800

    Add files via upload

---
+++ Scaling.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using System.Numerics;
+using Windows.Foundation;
+using Windows.UI.ViewManagement;
+
+namespace UWPtest1.Class
+{
+    class Scaling
+    {
+        public static void SetScale()
+        {
+            //Display Info
+            Rect bounds = ApplicationView.GetForCurrentView().VisibleBounds;
+            MainPage.scaledWidth = (float)bounds.Width / MainPage.designWidth;
+            MainPage.scaledHeight = (float)bounds.Height / MainPage.designHeight;
+        }
+
+        public static Transform2DEffect Img(CanvasBitmap src)
+        {
+            Transform2DEffect image;
+            image = new Transform2DEffect() { Source = src };
+            image.TransformMatrix = Matrix3x2.CreateScale(MainPage.scaledWidth, MainPage.scaledHeight);//Scale src image to X/Y dimensions 
+            return image;
+        }
+    }
+}
